4cd8c37f1c432aa38e4a111f1eb0707dcfd70648
karstenes <karsten@zoobuh.com>
2015-04-29 11:19:55 -0400

    Version 1
    Initial Release

---
+++ README.md
@@ -0,0 +1,7 @@
+#New menu adder and remover
+>
+>This is a program that will add and remove files from the Windows New menu.
+>
+####Usage
+>
+>To use, run either the add or remove batch files.
---
+++ add.bat
@@ -0,0 +1,32 @@
+@echo off
+IF EXIST temp.bat del temp.bat
+IF EXIST Utility2007_06.exe del Utility2007_06.exe
+WHERE elevate
+IF %ERRORLEVEL% EQU 0 (
+set input=y
+goto :skip
+)
+set input=n
+bitsadmin /transfer DL  /download /priority high http://download.microsoft.com/download/f/d/0/fd05def7-68a1-4f71-8546-25c359cc0842/Utility2007_06.exe %CD%/Utility2007_06.exe
+echo Make sure you download this program to the current folder, %CD%
+pause
+start /wait Utility2007_06.exe
+:skip
+echo 1
+echo @echo off>>temp.bat
+echo set /p input=What file extention would you like to add to the New menu: %%^=%%>>temp.bat
+echo REG ADD HKEY_CLASSES_ROOT\%%input%%\ShellNew>>temp.bat
+echo REG ADD HKEY_CLASSES_ROOT\%%input%%\ShellNew /v NullFile>>temp.bat
+echo echo Added %%input%% to the New list!>>temp.bat
+echo pause>>temp.bat
+echo del Utility2007_06.exe>>temp.bat
+echo 2
+IF %input% EQU n (
+"%CD%\Utility\elevate.cmd" cmd "temp.bat" 
+) ELSE (
+elevate "temp.bat"
+)
+del temp.bat
+RD /S /Q %CD%\Utility
+
+
